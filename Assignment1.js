d13f2a4a453d43d80935d6cee88e7b629dbbab14
muntazir7786 <146746098+muntazir7786@users.noreply.github.com>
2025-02-19 10:50:42 +0500

    Assignment1.js

---
+++ Assignment1.js
@@ -0,0 +1,28 @@
+var myAge;
+myAge ="17"
+var myName
+myName ="Muntazirr"
+var myAddress
+myAddress ="Garden East"
+
+document.write("YaY! I can write HTML content using js")
+
+
+alert(myName)
+alert(myAge)
+alert(myAddress)
+
+var userName = prompt("Enter Your Name")
+var productName = prompt("Enter Your Product")
+var quantity = prompt("Enter required quantity")
+
+
+document.write(userName+" "+"ordered"+" "+quantity+" "+productName+" "+"from xyz Store")
+
+var snack= "moserRothDark"
+var age= 17
+var maxAge= 102
+var perDay= 1
+var perYear= maxAge-age * perDay * 365
+
+document.write("<h1>The Lifetime Supply Calculator</h1> Current Age")

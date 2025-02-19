1d15b995f106d98661ee42274bbe8bf67d8473ea
muntazir7786 <146746098+muntazir7786@users.noreply.github.com>
2025-02-19 10:49:04 +0500

    Assignment4.js

---
+++ Assignment4.js
@@ -0,0 +1,24 @@
+
+document.write("hello world" + "<br>")
+document.write("hello world"+ "<br>")
+document.write("hello world"+ "<br>")
+document.write("hello world"+ "<br>")
+document.write("hello world"+ "<br>")
+
+for(i=1; i<=10; i++){
+    document.write(i+"<br>")
+}
+
+var userInput = +prompt("Enter Your Input")
+var tableLenght = +prompt("Enter Table Lenght")
+
+for(var i=1; i<=tableLenght  ; i++ ){
+    document.write(`${userInput} x ${i} = ${tableLenght*i}` + "<br>")
+}
+
+var A = ["Nokia", "Samsung", "Apple", "Sony", "Huawei"]
+
+for(var i=0; i< studentName.length; i++){
+    for(var j=0; j<Institute.length; j++)
+    document.write(studentName [i] + " " + Institute[j] + "<hr>")
+}

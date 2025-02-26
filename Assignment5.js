a8bcc240acffc72e493548b42fbdfe93807af65a
muntazir7786 <146746098+muntazir7786@users.noreply.github.com>
2025-02-26 10:43:54 +0500

    Assignment5.js

---
+++ Assignment5.js
@@ -0,0 +1,43 @@
+// var nose="muntazirr"
+// document.write(nose)
+
+// for(var views=1; views<1000000; views++) {
+//     document.write(views+"views"+"<br>")
+// }
+
+// var tableNum = +prompt("enter a number")
+// var tillNum = +prompt("enter a number till which you want to print the table")
+
+// for(var i=1; i<=tillNum  ; i++ ){
+//     document.write(${tableNum} x ${i} = ${tableNum8*i} <br>)
+// }
+
+// var studentName = ["muntazir", "hasnain", "sak", "rai", "rab"]
+// var Institute = ["NED", "Saylani"]
+
+// for(var i=0; i< studentName.length; i++){
+//     for(var j=0; j<Institute.length; j++)
+//     document.write(studentName [i] + " " + Institute[j] + "<hr>")
+// }
+
+// var cars= ["Mehran", "Audi", "Civic", "Lamborghini", "Toyota"]
+// var input = prompt("Enter Your Car");
+// var Values = false;
+// var copyFirst = input.slice(0,1).toUpperCase()
+// var copyAll = input.slice(1).toLowerCase()
+// var conc = copyFirst + copyAll;
+
+// for( var i=0; i<cars.length; i++){ 
+//     if(conc == cars[i] ){
+//         Values = true;
+//         document.write(cars[i] +  " " + "Car" + " " + "Found");
+//     }
+// }
+// if( Values === false){
+//     document.write("Car not found");
+// }
+
+var str = "Pakistan";
+str.split("a")
+
+document.write(str)

87dc3b169794c79fa02909a803c5f447295d6654
muntazir7786 <146746098+muntazir7786@users.noreply.github.com>
2025-02-28 10:55:13 +0500

    Create Assignment6.js

---
+++ Assignment6.js
@@ -0,0 +1,68 @@
+
+// ASSIGNMENT 6 
+
+// var userInput = +prompt("enter any positive integer")
+// var roundOff = Math.round(userInput)
+// var floorValue = Math.floor(userInput)
+// var ceilValue = Math.ceil(userInput)
+// document.write( "number is" + " " + userInput + "<br>")
+// document.write("round off value is " + roundOff + "<br>")
+// document.write("floor value is " + floorValue + "<br>")
+// document.write("ceil value is " + ceilValue + "<br>")
+
+// var userInput = +prompt("enter any negative integer")
+// var roundOff = Math.round(userInput)
+// var floorValue = Math.floor(userInput)
+// var ceilValue = Math.ceil(userInput)
+// document.write( "number is" + " " + userInput + "<br>")
+// document.write("round off value is " + roundOff + "<br>")
+// document.write("floor value is " + floorValue + "<br>")
+// document.write("ceil value is " + ceilValue + "<br>")
+
+
+// var userInput = +prompt("enter any positive floating point")
+// var roundOff = Math.round(userInput)
+// var floorValue = Math.floor(userInput)
+// var ceilValue = Math.ceil(userInput)
+// document.write( "number is" + " " + userInput + "<br>")
+// document.write("round off value is " + roundOff + "<br>")
+// document.write("floor value is " + floorValue + "<br>")
+// document.write("ceil value is " + ceilValue + "<br>")
+  
+
+// var userInput = +prompt("enter any negative floating point")
+// var roundOff = Math.round(userInput)
+// var floorValue = Math.floor(userInput)
+// var ceilValue = Math.ceil(userInput)
+// document.write( "number is" + " " + userInput + "<br>")
+// document.write("round off value is " + roundOff + "<br>")
+// document.write("floor value is " + floorValue + "<br>")
+// document.write("ceil value is " + ceilValue + "<br>")
+
+
+// var diceValue = Math.floor(Math.random() * 6) + 1;
+// document.write("You rolled: " + diceValue);
+
+// var coinValue = Math.floor(Math.random() * 2) + 1;
+// document.write("Output Of Toss: " + coinValue +"<br>");
+// if(coinValue == 1){document.write("random coin values : Heads")}
+// else if (coinValue ==2 ){document.write("random coin values : Tails")};
+
+// var Value = Math.floor(Math.random() * 100) + 1;
+// document.write("random number between 1 and 100: " + Value);
+
+// var user = prompt("your weight")
+// var user = parseFloat(user)
+// document.write( "your weight is " + user +" "+ "kg" + "<br>")
+
+// var input = prompt("enter the secret number")
+// var secret = 4
+// if (input == secret){document.write("congratulations you guessed it right")}
+// else if (input < secret){document.write("too low")}
+// else if (input > secret){document.write("too high")};
+
+// var date= new Date()
+// document.write(date)
+
+// var month= new Date()
+// document.write ("current month is " + month.toLocaleString('default', {month: 'long'}))
